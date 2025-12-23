70d59245a596a398a48646d0437a5fee109bf225
Swapnil B <bhosaleswapnil341@gmail.com>
2025-12-23 19:30:48 +0530

    Nth Fibonacci Number | .gitignore file added

---
+++ src/recursion/NthFibonacci.java
@@ -0,0 +1,20 @@
+package stack;
+import java.util.Scanner;
+public class NthFibonacci {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Number: ");
+		int n = sc.nextInt();
+		int result = calFibonacci(n);
+		System.out.println(n+"th Fibonacci number is: "+result);
+
+	}
+	
+	public static int calFibonacci(int no){
+		if(no == 1)	return 1;
+		if(no == 0) return 0;
+		return calFibonacci(no - 1) + calFibonacci(no - 2);
+	}
+
+}
